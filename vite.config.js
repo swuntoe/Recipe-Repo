a7124c0e8e0f4c5f6846e2820d3f5ae7cbb501de
minche.hsieh <swuntoe@gmail.com>
2026-02-25 15:41:48 +0100

    try to fix deployment errors

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 改成 '/Recipe-Repo/' 如果要部署到 GitHub Pages 的子路徑
+  base: '/Recipe-Repo/',
   build: {
     outDir: 'dist',
     sourcemap: false,
